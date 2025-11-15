6c8e85d7fe5bebf56e15aadf99f1c88d764c5404
Emre ÇOLAK <emrecolak@Emres-MacBook-Pro.local>
2025-11-15 22:46:02 +0300

    test commit hooks

--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -11,9 +11,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    })
+  );
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
