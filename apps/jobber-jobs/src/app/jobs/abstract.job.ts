9adf6fc8b11af70442570b5fcf79f2a4b38b689d
mjoe <colakkemre@gmail.com>
2025-11-18 23:25:19 +0300

    feat: implement data validation for jobs, refactor Fibonacci job data structure, and remove obsolete interface

--- apps/jobber-jobs/src/app/jobs/abstract.job.ts
+++ apps/jobber-jobs/src/app/jobs/abstract.job.ts
@@ -1,18 +1,32 @@
 import { Producer } from 'pulsar-client';
 import { PulsarClient } from '@jobber/pulsar';
 import { serialize } from '@jobber/pulsar';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+import { BadRequestException } from '@nestjs/common';
 
-export abstract class AbstractJob<T> {
+export abstract class AbstractJob<T extends object> {
   private producer: Producer;
+  protected abstract messageClass: new () => T;
 
   constructor(private readonly pulsarClient: PulsarClient) {}
 
   async execute(data: T, job: string) {
+    await this.validateData(data);
     if (!this.producer) {
       this.producer = await this.pulsarClient.createProducer(job);
     }
     await this.producer.send({
       data: serialize(data),
     });
+  }
+
+  private async validateData(data: T) {
+    const errors = await validate(plainToInstance(this.messageClass, data));
+    if (errors.length > 0) {
+      throw new BadRequestException(
+        `Job data is invalid: ${JSON.stringify(errors)}`
+      );
+    }
   }
 }
--- apps/jobber-jobs/src/app/jobs/fibonacci/fibonacci.jobs.ts
+++ apps/jobber-jobs/src/app/jobs/fibonacci/fibonacci.jobs.ts
@@ -1,13 +1,15 @@
 import { PulsarClient } from '@jobber/pulsar';
 import { Job } from '../../decorators/job.decorator';
 import { AbstractJob } from '../abstract.job';
-import { FibonacciData } from './fibonacci-data.interface';
+import { FibonacciData } from './fibonacci-data.message';
 
 @Job({
   name: 'Fibonacci',
   description: 'Generates Fibonacci numbers and store it in the database',
 })
 export class FibonacciJob extends AbstractJob<FibonacciData> {
+  protected messageClass = FibonacciData;
+
   constructor(pulsarClient: PulsarClient) {
     super(pulsarClient);
   }
