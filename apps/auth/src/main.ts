4320861ca05789a0f25df28d7ae2b9bac7925ab6
mjoe <colakkemre@gmail.com>
2025-11-22 11:56:01 +0300

    feat: add AUTH_GRPC_SERVICE_URL to environment variables and deployment configuration

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -9,6 +9,7 @@ import { AppModule } from './app/app.module';
 import { GrpcOptions, Transport } from '@nestjs/microservices';
 import { AUTH_PACKAGE_NAME } from '@jobber/grpc';
 import { init } from '@jobber/nestjs';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
@@ -17,6 +18,7 @@ async function bootstrap() {
   app.connectMicroservice<GrpcOptions>({
     transport: Transport.GRPC,
     options: {
+      url: app.get(ConfigService).getOrThrow<string>('AUTH_GRPC_SERVICE_URL'),
       package: AUTH_PACKAGE_NAME,
       protoPath: join(__dirname, '../../libs/grpc/proto/auth.proto'),
     },
